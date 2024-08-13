88e33570411f81ed34b45847196620a15a9c61de
jasperw97 <jasperdeanwei@gmail.com>
2024-08-13 21:29:07 +0800

    cors netlify 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -159,7 +159,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://fitness-app-on-render-1.onrender.com",
-    "https://fittogetherr.netlify.app "
+    "https://fittogetherr.netlify.app"
 ]
 
 REST_FRAMEWORK = {
